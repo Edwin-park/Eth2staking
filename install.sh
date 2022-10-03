1aa0165d50930d293bfd7d35ccbadd06a51567c6
Edwin-park <85816990+Edwin-park@users.noreply.github.com>
2022-10-03 23:50:39 +0900

    클라이언트 버전 자동확인

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash 
 
+
+echo ""
+echo ""
+echo "---------------------------------"
+echo -e "  자동 설치 ver : ""\033[32m"22.10.03"\033[0m"""
+echo ""
+echo "  클라이언트 버전 자동확인"
+echo "---------------------------------"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+
 echo "설치하실 네트워크를 선택해주세요"
 echo "1 : Prater Testnet"
 echo "2 : Main net"
@@ -21,17 +37,32 @@ echo -n " ex : [ 0xAa83d6C8A07492a28Af2DfDb57Fe69306362f02E ] : "
 read address
 
 
+# Check the latest versions of clients
+	lst_vGE1=$(curl -s -I https://github.com/ethereum/go-ethereum/releases/latest | grep tag)
+	lst_vGE2=${lst_vGE1#*tag/v}
+	lst_vGE=$(echo $lst_vGE2 | sed 's/\r$//')
 
-echo "클라이언트 버전을 입력해주세요. "
+	lst_vLH1=$(curl -s -I https://github.com/sigp/lighthouse/releases/latest | grep tag)
+	lst_vLH2=${lst_vLH1#*tag/v}
+	lst_vLH=$(echo $lst_vLH2 | sed 's/\r$//')
 
-echo -n "Geth 버전 입력하세요        (ex : 1.10.25) : "
-read Geth
+	lst_vME1=$(curl -s -I https://github.com/flashbots/mev-boost/releases/latest | grep tag)
+	lst_vME2=${lst_vME1#*tag/v}
+	lst_vME=$(echo $lst_vME2 | sed 's/\r$//')
 
-echo -n "Lighthouse 버전 입력하세요  (ex : 3.1.0)   : "
-read Lighthouse
 
-echo -n "Mev-Boost 버전 입력하세요   (ex : 1.3.2)   : "
-read Mev
+
+# Client selection
+	echo ""
+	echo " 설치할ver"
+	echo "---------------------------------------------------------------"
+	echo -e "\033[0m""  1. geth (\033[0m"" ver : ""\033[31;1m""$lst_vGE""\033[0m"")"
+	echo ""
+	echo -e "\033[0m""  2. lighthouse (\033[0m"" ver : ""\033[31;1m""$lst_vLH""\033[0m"")"
+	echo ""
+	echo -e "\033[0m""  3. MEV-boost (\033[0m"" ver : ""\033[31;1m""$lst_vME""\033[0m"")"
+	echo "---------------------------------------------------------------"
+	echo ""
 
 
 
@@ -41,9 +72,9 @@ echo "네트워크 및 클라이언트 버전 최종확인"
 echo ""
 echo "네트워크 : $network2"
 echo "Tip 수령주소 : $address"
-echo "Geth v$Geth"
-echo "Lighthous v$Lighthouse"
-echo "Mev-Boost v$Mev"
+echo "Geth v$lst_vGE"
+echo "Lighthous v$lst_vLH"
+echo "Mev-Boost v$lst_vME"
 echo ""
 
 
@@ -80,12 +111,12 @@ echo ""
 
 #Lighthouse 설치
 cd ~
-curl -LO https://github.com/sigp/lighthouse/releases/download/v"$Lighthouse"/lighthouse-v"$Lighthouse"-x86_64-unknown-linux-gnu.tar.gz
-tar xvf lighthouse-v"$Lighthouse"-x86_64-unknown-linux-gnu.tar.gz
+curl -LO https://github.com/sigp/lighthouse/releases/download/v"$lst_vLH"/lighthouse-v"$lst_vLH"-x86_64-unknown-linux-gnu.tar.gz
+tar xvf lighthouse-v"$lst_vLH"-x86_64-unknown-linux-gnu.tar.gz
 sudo cp lighthouse /usr/local/bin
-sudo rm lighthouse lighthouse-v"$Lighthouse"-x86_64-unknown-linux-gnu.tar.gz
+sudo rm lighthouse lighthouse-v"$lst_vLH"-x86_64-unknown-linux-gnu.tar.gz
 echo ""
-echo "Lighthouse 설치완료"
+echo "Lighthouse v"$lst_vLH" 설치완료"
 echo ""
 
 #비콘체인 설치
@@ -105,14 +136,14 @@ sudo chmod 700 /var/lib/lighthouse/validators
 #MEV-Boost 설치
 sudo useradd --no-create-home --shell /bin/false mevboost
 cd ~
-curl -LO https://github.com/flashbots/mev-boost/releases/download/v"$Mev"/mev-boost_"$Mev"_linux_amd64.tar.gz
-tar xvf mev-boost_"$Mev"_linux_amd64.tar.gz
+curl -LO https://github.com/flashbots/mev-boost/releases/download/v"$lst_vME"/mev-boost_"$lst_vME"_linux_amd64.tar.gz
+tar xvf mev-boost_"$lst_vME"_linux_amd64.tar.gz
 sudo cp mev-boost /usr/local/bin
 sudo chown mevboost:mevboost /usr/local/bin/mev-boost
 cd ~
-sudo rm mev-boost LICENSE README.md mev-boost_"$Mev"_linux_amd64.tar.gz
+sudo rm mev-boost LICENSE README.md mev-boost_"$lst_vME"_linux_amd64.tar.gz
 echo ""
-echo "Mev-Boost 설치완료"
+echo "Mev-Boost v"$lst_vME" 설치완료"
 echo ""
 
 #Prater Testnet 서비스파일
@@ -121,7 +152,6 @@ cat > /etc/systemd/system/geth.service << EOF
 [Unit]
 Description=Geth
 After=network-online.target
-
 [Service]
 Type=simple
 User=geth
@@ -136,10 +166,8 @@ ExecStart=geth \\
   --port=10001 \\
   --authrpc.jwtsecret /var/lib/jwtsecret/jwt.hex \\
   --authrpc.vhosts="*"
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -147,12 +175,10 @@ cat > /etc/systemd/system/lighthousebeacon.service << EOF
 [Unit]
 Description=Lighthouse Beacon Node
 After=geth.target
-
 [Service]
 Type=simple
 User=lighthousebeacon
 Group=lighthousebeacon
-
 ExecStart=/usr/local/bin/lighthouse bn \\
   --network prater \\
   --datadir /var/lib/lighthouse \\
@@ -166,10 +192,8 @@ ExecStart=/usr/local/bin/lighthouse bn \\
   --discovery-port 9001 \\
   --checkpoint-sync-url https://26nBBRbB6LXwiG9j68YEx0jJ9zE:d32d9dd0eaecadb8a99b7fa6443a19e8@eth2-beacon-prater.infura.io \\
   --builder http://localhost:18550
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -177,12 +201,10 @@ cat > /etc/systemd/system/lighthousevalidator.service << EOF
 [Unit]
 Description=Lighthouse Validator Node
 After=lighthousebeacon.target
-
 [Service]
 Type=simple
 User=lighthousevalidator
 Group=lighthousevalidator
-
 ExecStart=/usr/local/bin/lighthouse vc \\
   --network prater \\
   --beacon-nodes http://localhost:5052 \\
@@ -190,10 +212,8 @@ ExecStart=/usr/local/bin/lighthouse vc \\
   --metrics \\
   --suggested-fee-recipient $address \\
   --builder-proposals
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -201,20 +221,16 @@ cat > /etc/systemd/system/mevboost.service << EOF
 [Unit]
 Description=mev-boost
 After=network-online.target
-
 [Service]
 Type=simple
 User=mevboost
 Group=mevboost
-
 ExecStart=mev-boost \\
     -goerli \\
     -relay-check \\
     -relays https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@builder-relay-goerli.flashbots.net,https://0x821f2a65afb70e7f2e820a925a9b4c80a159620582c1766b1b09729fec178b11ea22abb3a51f07b288be815a1a2ff516@bloxroute.max-profit.builder.goerli.blxrbdn.com,https://0x8f7b17a74569b7a57e9bdafd2e159380759f5dc3ccbd4bf600414147e8c4e1dc6ebada83c0139ac15850eb6c975e82d0@builder-relay-goerli.blocknative.com,https://0xaa1488eae4b06a1fff840a2b6db167afc520758dc2c8af0dfb57037954df3431b747e2f900fe8805f05d635e9a29717b@relay-goerli.edennetwork.io
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -227,7 +243,6 @@ cat > /etc/systemd/system/geth.service << EOF
 [Unit]
 Description=Geth
 After=network-online.target
-
 [Service]
 Type=simple
 User=geth
@@ -241,10 +256,8 @@ ExecStart=geth \\
   --port=20001 \\
   --authrpc.jwtsecret /var/lib/jwtsecret/jwt.hex \\
   --authrpc.vhosts="*"
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -252,12 +265,10 @@ cat > /etc/systemd/system/lighthousebeacon.service << EOF
 [Unit]
 Description=Lighthouse Beacon Node
 After=geth.target
-
 [Service]
 Type=simple
 User=lighthousebeacon
 Group=lighthousebeacon
-
 ExecStart=/usr/local/bin/lighthouse bn \\
   --network mainnet \\
   --datadir /var/lib/lighthouse \\
@@ -271,10 +282,8 @@ ExecStart=/usr/local/bin/lighthouse bn \\
   --discovery-port 9002 \\
   --checkpoint-sync-url https://26pgNISME52yCryWfyA0jNQPYDe:80d95be613e437ff37ee8024f2fb4d4e@eth2-beacon-mainnet.infura.io \\
   --builder http://localhost:18550
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -282,12 +291,10 @@ cat > /etc/systemd/system/lighthousevalidator.service << EOF
 [Unit]
 Description=Lighthouse Validator Node
 After=lighthousebeacon.target
-
 [Service]
 Type=simple
 User=lighthousevalidator
 Group=lighthousevalidator
-
 ExecStart=/usr/local/bin/lighthouse vc \\
   --network mainnet \\
   --beacon-nodes http://localhost:5052 \\
@@ -295,10 +302,8 @@ ExecStart=/usr/local/bin/lighthouse vc \\
   --metrics \\
   --suggested-fee-recipient $address \\
   --builder-proposals
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -306,20 +311,16 @@ cat > /etc/systemd/system/mevboost.service << EOF
 [Unit]
 Description=mev-boost
 After=network-online.target
-
 [Service]
 Type=simple
 User=mevboost
 Group=mevboost
-
 ExecStart=mev-boost \\
     -mainnet \\
     -relay-check \\
     -relays https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net,https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com,https://0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246@builder-relay-mainnet.blocknative.com,https://0xb3ee7afcf27f1f1259ac1787876318c6584ee353097a50ed84f51a1f21a323b3736f271a895c7ce918c038e4265918be@relay.edennetwork.io
-
 Restart=always
 RestartSec=5
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -342,7 +343,8 @@ sudo systemctl start lighthousebeacon
 
 echo "geth 로그 보는법 : g.log"
 echo "beacon 로그 보는법 : lb.log"
-
+echo ""
+echo "4단계 이어서 진행하시면 됩니다!"
 
 
 
@@ -358,8 +360,3 @@ else
   exit
 
 fi
-
-
-
-
-
