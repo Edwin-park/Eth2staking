28cfb93127201e318588909409ce5565982b5c38
Edwin-park <85816990+Edwin-park@users.noreply.github.com>
2022-12-24 12:51:53 +0900

    Mev 릴레이 주소변경 (메인넷)
    - 릴레이 주소(참고)
        
        [https://github.com/eth-educators/ethstaker-guides/blob/main/MEV-relay-list.md](https://ethstaker.cc/mev-relay-list/)
        
        <Flashbots>
        
        https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net
        
        <BloXroute - Max profit>
        
        https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com
        
        <Aestus>
        
        https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com
        
        <Agnostic Gnosis>
        
        https://0xa7ab7a996c8584251c8f925da3170bdfd6ebc75d50f5ddc4050a6fdc77f2a3b5fce2cc750d0865e05d7228af97d69561@agnostic-relay.net
        
        <Ultra Sound>
        
        https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay.ultrasound.money

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 echo ""
 echo ""
 echo "---------------------------------"
-echo -e "  자동 설치 ver : ""\033[32m"22.11.21"\033[0m"""
+echo -e "  자동 설치 ver : ""\033[32m"22.12.24"\033[0m"""
 echo ""
-echo "  Mev 릴레이 주소변경 "
+echo "  Mev 릴레이 주소변경(5개) "
 echo ""
 echo "---------------------------------"
 echo ""
@@ -307,7 +307,7 @@ Group=mevboost
 ExecStart=mev-boost \\
     -mainnet \\
     -relay-check \\
-    -relays https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net,https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com,https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com,https://0x84e78cb2ad883861c9eeeb7d1b22a8e02332637448f84144e245d20dff1eb97d7abdde96d4e7f80934e5554e11915c56@relayooor.wtf
+    -relays https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net,https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com,https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com,https://0xa7ab7a996c8584251c8f925da3170bdfd6ebc75d50f5ddc4050a6fdc77f2a3b5fce2cc750d0865e05d7228af97d69561@agnostic-relay.net,https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay.ultrasound.money
 Restart=always
 RestartSec=5
 [Install]
