d2a6d85b4b58d08b70d70ad0060fcfcfe1d15b21
Edwin-park <85816990+Edwin-park@users.noreply.github.com>
2023-04-19 22:14:47 +0900

    Update update2.sh

--- update2.sh
+++ update2.sh
@@ -8,21 +8,16 @@ echo -e "  자동 업데이트 ver : ""\033[32m"23.04.19"\033[0m"""
 echo "---------------------------------"
 echo ""
 echo ""
-echo ""
-echo ""
-echo ""
 echo "설치된 버전 확인중..."
 echo ""
 
 
 # Check versions of installed clients
-	ins_vR1=$(rocketpool -V | grep version)
+	ins_vR1=$(/home/staker/bin/rocketpool -v | grep version)
 	ins_vR=${ins_vR1#*version }
-	
 
 
 
-https://github.com/rocket-pool/smartnode-install/releases/latest
 
 # Check the latest versions of clients
 	lst_vR1=$(curl -s -I https://github.com/rocket-pool/smartnode-install/releases/latest | grep tag)
@@ -33,8 +28,8 @@ https://github.com/rocket-pool/smartnode-install/releases/latest
 
 # Client selection
 	echo ""
-	echo " 현재ver / 설치할ver"
 	echo "---------------------------------------------------------------"
+	echo ""
 	echo -e "\033[0m""  RocketPool (현재ver : ""\033[32m""$ins_vR""\033[0m"" / 설치할ver : ""\033[31;1m""$lst_vR""\033[0m"")"
 	echo ""
 	echo "---------------------------------------------------------------"
@@ -44,75 +39,29 @@ https://github.com/rocket-pool/smartnode-install/releases/latest
 
 
 
-echo "업데이트 할 클라이언트를 선택해주세요"
-echo ""
-echo "1 : Geth + 시스템전체"
-echo "2 : Lighthous"
-echo "3 : Mev-Boost"
-echo ""
-echo -n "[1,2,3] 엽력 : "
-read select
-
 
 
 echo ""
 
-read -p " [ "$select" ] 선택하셨습니다. 업데이트 시작할까요? [y , n] : " check
+echo " 로켓풀 서비스는 중지되고, 시스템 및 로켓풀 업데이트후 재 시작됩니다. "
+read -p " 업데이트를 시작할까요? [y , n] : " check
 
 
 if [ "$check" = "y" ] ; then 
   echo "업데이트를 시작합니다... "
 
-  if [ "$select" = "1" ] ; then 
-    sudo systemctl stop geth
-    sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
-    sudo systemctl start geth
-    echo ""
-    echo ""
-    ins_vGE1n=$(geth version | grep Version)
-    ins_vGE2n=${ins_vGE1n%-stable*}
-    ins_vGEn=${ins_vGE2n#*Version: }
+  sudo /home/staker/bin/rocketpool service stop
+  sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+  wget https://github.com/rocket-pool/smartnode-install/releases/latest/download/rocketpool-cli-linux-amd64 -O /home/staker/bin/rocketpool
+  sudo /home/staker/bin/rocketpool service start
+  echo ""
+  echo ""
+  echo "시스템 전체(보안) 업데이트 완료!"
+  echo ""
+  echo "RocketPool v"$lst_vR" 업데이트 완료!"
+  echo ""
     
-    echo "Geth v"$ins_vGEn" 및 시스템전체 업데이트 완료!"
-    echo ""
-    
-  elif [ "$select" = "2" ] ; then 
-    cd ~
-    curl -LO https://github.com/sigp/lighthouse/releases/download/v"$lst_vLH"/lighthouse-v"$lst_vLH"-x86_64-unknown-linux-gnu.tar.gz
-    tar xvf lighthouse-v"$lst_vLH"-x86_64-unknown-linux-gnu.tar.gz
-    sudo systemctl stop lighthousevalidator
-    sudo systemctl stop lighthousebeacon
-    sudo cp lighthouse /usr/local/bin
-    sudo rm lighthouse lighthouse-v"$lst_vLH"-x86_64-unknown-linux-gnu.tar.gz
-    sudo systemctl start lighthousebeacon
-    sudo systemctl start lighthousevalidator
-    echo ""
-    echo ""
-    ins_vLH1n=$(lighthouse --version | grep "Lighthouse v")
-    ins_vLH2n=${ins_vLH1n#*v}
-    ins_vLHn=${ins_vLH2n%-*}
-    
-    echo "Lighthouse v"$ins_vLHn" 업데이트 완료!"
-    echo ""
-    
-  elif [ "$select" = "3" ] ; then 
-    cd ~
-    curl -LO https://github.com/flashbots/mev-boost/releases/download/v"$lst_vME"/mev-boost_"$lst_vME"_linux_amd64.tar.gz
-    tar xvf mev-boost_"$lst_vME"_linux_amd64.tar.gz
-    sudo systemctl stop mevboost
-    sudo cp mev-boost /usr/local/bin
-    sudo chown mevboost:mevboost /usr/local/bin/mev-boost
-    sudo rm mev-boost LICENSE README.md mev-boost_"$lst_vME"_linux_amd64.tar.gz
-    sudo systemctl start mevboost
-    echo ""
-    echo ""
-    ins_vME1n=$(mev-boost --version | grep mev-boost)
-    ins_vMEn=${ins_vME1n#*boost }
-    echo "Mev-Boost v"$ins_vMEn" 업데이트 완료!"
-    echo ""
-  else
-    exit
-fi
+
 
 
 
