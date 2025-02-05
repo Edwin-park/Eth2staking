ba3c62f7ac062c55835cbe68590ff2c48e190be4
SUNGBACK <85816990+Edwin-park@users.noreply.github.com>
2025-02-05 13:10:34 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -24,7 +24,7 @@ echo ""
 	ins_vLH2=${ins_vLH1#*v}
 	ins_vLH=${ins_vLH2%-*}
 
-	ins_vME1=$(mev-boost --version | awk '{for(i=1;i<=NF;i++) if($i ~ /mev-boost/) print $(i+1)}')
+	ins_vME1=$(mev-boost --version | grep -Eo 'mev-boost [0-9.]+' | awk '{print $2}')
 	ins_vME=${ins_vME1#*boost }
 
 
